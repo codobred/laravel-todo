2a5dd383f22ef2840bad89814401d99ef0c5b6e2
Leonid Marchenko <codobred@gmail.com>
2017-06-29 19:02:12 +0300

    update import view

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.notes')
 
 @section('content')
-    <div class="well">
+    <div class="row">
         <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
             {{ csrf_field() }}
             <div class="form-group col-xs-12">
-                <input type="file" name="file" class="form-control">
+                <label for="file">Выберите файл в формате txt или xml</label>
+                <input id="file" type="file" name="file" class="form-control">
             </div>
             <div class="form-group col-xs-12 text-right">
                 <button class="btn btn-primary" type="submit">Импортировать</button>
